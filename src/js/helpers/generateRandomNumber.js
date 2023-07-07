1fef94117cdd9ff9a165be1ad5929c6fbd5b903d
Karol Wolski <karolwolski04@gmail.com>
2023-07-07 21:33:30 +0200

    feat: add section with image (#3)

---
+++ src/js/helpers/generateRandomNumber.js
@@ -0,0 +1,2 @@
+export const generateRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min)) + min;
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+import { createCard } from "./card";
+
+const init = () => {
+  const obj = {
+    title: "Lorem Ipsum",
+    desc: "Infinitely scalable, feature-rich and cloud-native data management and protection for modern and legacy infrastructures and SaaS platforms, managed via a single app with no hardware required.",
+    img: "sean-o-KMn4VEeEPR8-unsplash_1_s6zmfh_ar_16_9,c_fill,g_auto__c_scale,w_596.jpg",
+    imgAltText: "This is a sunset landscape.",
+  };
+  createCard(obj);
+  createCard(obj);
+};
+
+init();
